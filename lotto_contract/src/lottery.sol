456b9e79d86b457dda6cee69a287d40760b59aea
dadsec-dev <geecypher41@gmail.com>
2024-06-29 18:17:21 +0100

    added xp points, all draws return function, check winner function, and some other helper read functions

--- lotto_contract/src/lottery.sol
+++ lotto_contract/src/lottery.sol
@@ -8,32 +8,38 @@ contract Lottery is GelatoVRFConsumerBase {
     address public teamWallet;
     uint256 public ticketPrice = 0.01 ether;
     uint256 public drawDuration = 30 minutes;
-    @audit "lets put this into the draw struct to track every draw pot per draw"
-    uint256 public drawPot = 0;
-    address[] public winners;
-    uint256 public totalxp;
     uint256 public totalSuperBowlFund;
 
     struct Draw {
         uint256 endTime;
-        mapping(uint256 => address) participants;
+        uint256 drawPot;
         uint8[] chosenNumbers;
         address[] winners;
-        
+        bool isOpen;
+        address[] participantsArray;
+        mapping(uint256 => address) participants;
+    }
+
+    struct DrawInfo {
+        uint256 endTime;
+        uint256 drawPot;
+        uint8[] chosenNumbers;
+        address[] winners;
+        bool isOpen;
+        address[] participantsArray;
     }
 
     mapping(uint256 => Draw) public draws;
+    mapping(address => uint256) public userXP;
     uint256 public drawCount;
-    @audit "use mapping"
     address[] public superBowlParticipants;
-
 
     event TicketPurchased(address indexed participant, uint8 number, uint256 drawNumber);
     event DrawEnded(uint256 drawNumber, address[] winners, uint256 prize);
     event SuperBowlDrawEnded(address winner, uint256 prize);
 
     error NotModerator();
-    error _drawEnded();
+    error drawEnded();
     error IncorrectTicketPrice();
     error InvalidNumber();
     error NumberAlreadyChosen();
@@ -54,23 +60,24 @@ contract Lottery is GelatoVRFConsumerBase {
     function _startNewDraw() internal {
         drawCount++;
         draws[drawCount].endTime = block.timestamp + drawDuration;
+        draws[drawCount].isOpen = true;
     }
 
     function startNewDraw() public onlyModerator {
         _startNewDraw();
     }
 
-function fulfillRandomness(uint256 randomness, uint256 requestId, bytes memory extraData) public onlyModerator {
+    function fulfillRandomness(uint256 randomness, uint256 requestId, bytes memory extraData) public onlyModerator {
         _fulfillRandomness(randomness, requestId, extraData);
-}
+    }
 
-function _operator() view override internal returns (address){
-    return moderator;
-}
+    function _operator() view override internal returns (address) {
+        return moderator;
+    }
 
-function buyTicket(uint8 number) external payable {
+    function buyTicket(uint8 number) external payable {
         if (block.timestamp >= draws[drawCount].endTime) {
-            revert _drawEnded();
+            revert drawEnded();
         }
         if (msg.value != ticketPrice) {
             revert IncorrectTicketPrice();
@@ -86,55 +93,88 @@ function buyTicket(uint8 number) external payable {
 
         currentDraw.participants[number] = msg.sender;
         currentDraw.chosenNumbers.push(number);
-        drawPot += msg.value; @audit "lets put this into the draw struct to track every draw pot per draw";
+        currentDraw.drawPot += msg.value;
+        currentDraw.participantsArray.push(msg.sender);
 
         superBowlParticipants.push(msg.sender);
+
+        userXP[msg.sender] += 10;
 
         emit TicketPurchased(msg.sender, number, drawCount);
     }
 
-function endDraw() external onlyModerator {
+    function endDraw() external onlyModerator {
         require(block.timestamp >= draws[drawCount].endTime, "Draw has not ended yet");
 
         Draw storage currentDraw = draws[drawCount];
 
         // Logic to select winners using Gelato VRF (omitted for simplicity)
         address[] memory winners = new address[](3); // Assume winners are selected here
-        
-        drawPot = 0;
-        uint256 prize = (drawPot * 80) / 100;
+
+        uint256 prize = (currentDraw.drawPot * 80) / 100;
 
         for (uint256 i = 0; i < winners.length; i++) {
-            @audit "use call to transfeer"
-            payable(winners[i]).transfer(prize / winners.length);
+            // Use call to transfer
+            (bool sent, ) = winners[i].call{value: prize / winners.length}("");
+            require(sent, "Failed to send Ether");
         }
 
-        uint256 platformFee = (drawPot * 5) / 100;
-        @audit "use call to transfeer"
-        payable(teamWallet).transfer(platformFee);
+        uint256 platformFee = (currentDraw.drawPot * 5) / 100;
+        // Use call to transfer
+        (bool sentFee, ) = teamWallet.call{value: platformFee}("");
+        require(sentFee, "Failed to send Ether");
 
-        uint256 superBowlFee = (drawPot * 15) / 100;
-        // Assume SuperBowl fund logic here
+        uint256 superBowlFee = (currentDraw.drawPot * 15) / 100;
+        totalSuperBowlFund += superBowlFee;
 
         emit DrawEnded(drawCount, winners, prize);
 
-        @audit "follow check effect interaction can cause reenrancy, do a check that the draw is zero before the loop for winner transfer"
-        drawPot = 0;
+        currentDraw.isOpen = false;
         _startNewDraw();
-}
+    }
 
-function endSuperBowlDraw() external onlyModerator {
+        function endSuperBowlDraw() external onlyModerator {
         require(superBowlParticipants.length > 0, "No participants for SuperBowl draw");
 
         // Logic to select SuperBowl winner using Gelato VRF (omitted for simplicity)
         address winner = superBowlParticipants[0]; // Assume winner is selected here
 
         uint256 superBowlPrize = address(this).balance; // Assuming the prize is the remaining balance
-        payable(winner).transfer(superBowlPrize);
+        (bool sent, ) = winner.call{value: superBowlPrize}("");
+        require(sent, "Failed to send Ether");
 
         emit SuperBowlDrawEnded(winner, superBowlPrize);
 
-        @audit "move it above"
         delete superBowlParticipants;
+    }
+
+    function checkIfWinner(address _address) external view returns (bool) {
+        for (uint256 i = 1; i <= drawCount; i++) {
+            Draw storage draw = draws[i];
+            for (uint256 j = 0; j < draw.winners.length; j++) {
+                if (draw.winners[j] == _address) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function returnDraws() external view returns (DrawInfo[] memory) {
+        DrawInfo[] memory allDraws = new DrawInfo[](drawCount);
+
+        for (uint256 i = 1; i <= drawCount; i++) {
+            Draw storage draw = draws[i];
+            DrawInfo memory drawInfo;
+            drawInfo.endTime = draw.endTime;
+            drawInfo.drawPot = draw.drawPot;
+            drawInfo.chosenNumbers = draw.chosenNumbers;
+            drawInfo.winners = draw.winners;
+            drawInfo.isOpen = draw.isOpen;
+            drawInfo.participantsArray = draw.participantsArray;
+            allDraws[i - 1] = drawInfo;
+        }
+
+        return allDraws;
     }
 }
